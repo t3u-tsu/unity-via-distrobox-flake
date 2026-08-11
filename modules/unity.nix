8fd614adc74a64946d92b2300b5d51b551505c43
t3u <t3u@t3u.uk>
2026-08-12 01:59:56 +0900

    fix: launch via systemctl --user start in desktop entry

--- modules/unity.nix
+++ modules/unity.nix
@@ -46,13 +46,13 @@ in
     xdg.configFile."distrobox/distrobox.ini".text = distroboxIni;
 
     # xdg.desktopEntries is broken in the current home-manager release.
-    # systemd-run --user escapes browser sandboxes for unityhub:// deep links.
+    # systemctl --user start escapes browser sandboxes for unityhub:// deep links.
     xdg.dataFile."applications/unityhub.desktop".text = ''
       [Desktop Entry]
       Type=Application
       Name=Unity Hub
       GenericName=Unity Hub Launcher
-      Exec=systemd-run --user --no-block unity-via-distrobox.service
+      Exec=systemctl --user start --no-block unity-via-distrobox.service
       Icon=unityhub
       MimeType=x-scheme-handler/unityhub;
       Categories=Development;
